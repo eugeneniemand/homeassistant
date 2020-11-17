6df2c2caa91957259e27b4ef2ae9b3b5b3b6f792
Eugene Niemand <eugene.niemand@gmail.com>
2020-11-17 12:10:47 +0000

    automater push on 17-11-2020 12:10:47

---
+++ ha_gitpush.sh
@@ -0,0 +1,10 @@
+sudo  chmod a+r  ./homeassistant/.storage/**
+
+# Add all files to the repository with respect to .gitignore rules
+git add .
+
+# Commit changes with message with current date stamp
+git commit -m "automater push on `date +'%d-%m-%Y %H:%M:%S'`"
+
+# Push changes towards GitHub
+git push -u origin master
