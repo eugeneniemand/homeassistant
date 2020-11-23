95ab06cfb2a412294392651a0f238b3f27ea48e0
Eugene Niemand <eugene.niemand@asos.com>
2020-11-23 15:50:01 +0000

    removed old instance

--- homeassistant_old/python_scripts/notification_engine.py
+++
@@ -1,9 +0,0 @@
-#params = data.get("params")
-def hasNumbers(inputString):
-    return any(char.isdigit() for char in inputString)
-
-lights = hass.states.entity_ids("light")
-hasNumbers("abc3bbb")
-#filtered_lights = [light for light in lights if hasNumbers(light)]
-
-logger.warning(filtered_lights)
